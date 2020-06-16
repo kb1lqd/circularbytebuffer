6670d6e3afffa9395c9699528a05e806f7301e98
Brenton Salmi <kb1lqd@gmail.com>
2020-06-16 00:03:07 -0700

    Adding first documentation.

--- ring.h
+++ ring.h
@@ -1,7 +1,25 @@
+/*
+* @file ring.h
+* @author Brenton Salmi, KB1LQD
+* @date 15 June, 2020
+* @brief A simple circular byte buffer implementation.
+*
+* This is a simple implementation of a byte circular buffer that stores and
+* retrieves data as a First-in-first-out (FIFO).
+*/
+
 #ifndef RING_H_
 #define RING_H_
 
-// Define circular buffer struct
+/**
+* @brief This is the main control logic struct to maintain FIFO paramters and state.
+*
+* A pointer to a fixed byte array and the buffer size (int) in bytes is
+* Initialized at program boot. During operations the logic states and values are
+* updated to reflect proper operation. In an overflow condition (no more room
+* left in buffer for new data) then the data is ignored, dropped, and overflowflag
+* is toggled to a value of 1.
+*/
 struct ring_buffer_control {
 	unsigned char * buffer;
 	unsigned int bufferbytesize;
@@ -11,12 +29,59 @@ struct ring_buffer_control {
 	unsigned char overflowflag;
 };
 
+/**
+* @brief Initializes a circular buffer control logic struct.
+*
+* This function initlizes a circular buffer instance to a known initial state
+* and the desired size in bytes.
+*
+* @param rbufcntl A pointer to the ring_buffer_control struct that the FIFO will use to store control logic state
+* @param rbuf A pointer to the byte array that the FIFO will use to store bytes
+* @param bufferbytesize Size in bytes (int) of the rbuf byte array
+* @return Void
+*/
 void RingInitBuffer(struct ring_buffer_control *rbufcntl, unsigned char *rbuf, unsigned int bufferbytesize);
+
+/**
+*
+*
+*
+*/
 int RingPut(struct ring_buffer_control *rbufcntl, unsigned char *rbuf, unsigned char *data, unsigned int length);
+
+/**
+*
+*
+*
+*/
 void RingPutIncrement(struct ring_buffer_control *rbufcntl);
+
+/**
+*
+*
+*
+*/
 int RingGet(struct ring_buffer_control *rbufcntl, unsigned char *rbuf, unsigned char *getbuf, unsigned int length);
+
+/**
+*
+*
+*
+*/
 void RingGetIncrement(struct ring_buffer_control *rbufcntl);
+
+/**
+*
+*
+*
+*/
 int RingInWaiting(struct ring_buffer_control *rbufcntl);
+
+/**
+*
+*
+*
+*/
 int RingResetOverflow(struct ring_buffer_control *rbufcntl);
 
 #endif
